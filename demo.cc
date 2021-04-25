b96d3ced72061116bd563559f98abab25a4072b3
YAO <37959723+YYC0626@users.noreply.github.com>
2021-04-25 16:20:49 +0800

    Update demo.cc

--- demo.cc
+++ demo.cc
@@ -2,10 +2,10 @@
 #include <stddef.h>
 
 bool FuzzMe(const uint8_t *Data, size_t DataSize) {
-//  return DataSize >= 3 && Data[0] == 'F' && Data[1] == 'U' && Data[2] == 'Z' && Data[3] == 'Z';  // :‑<
+  return DataSize >= 3 && Data[0] == 'F' && Data[1] == 'U' && Data[2] == 'Z' && Data[3] == 'Z';  // :‑<
   //未修改
 
-  return DataSize  ;
+//  return DataSize  ;
   //修改后
 }
 
